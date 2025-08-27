d8bf07ffaa2d482a28dd127375c4cb278e7f0ae5
Hasitha Gallella <hbgallella@gmail.com>
2025-08-27 14:45:42 +0530

    Base configuration

---
+++ Software_design_competition/src/main/java/com/uom/Software_design_competition/SoftwareDesignCompetitionApplication.java
@@ -0,0 +1,13 @@
+package com.uom.Software_design_competition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareDesignCompetitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareDesignCompetitionApplication.class, args);
+	}
+
+}
